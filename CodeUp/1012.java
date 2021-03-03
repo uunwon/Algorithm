8742a450b5743d710e68a3f8232ed9db6acaec2f
yunwoon <chea2089@naver.com>
2021-03-03 15:34:58 +0900

    [codeup] 입출력 실수 1개

---
+++ CodeUp/1012.java
@@ -0,0 +1,11 @@
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		float f = scanner.nextFloat();
+		
+		System.out.println(f);
+	}
+}
