d89edfb44c427efd83f09fe11c49a75427af243a
yunwoon <chea2089@naver.com>
2021-03-03 15:32:59 +0900

    [codeup] 입출력 정수 1개

---
+++ CodeUp/1010.java
@@ -0,0 +1,11 @@
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		System.out.println(num);
+	}
+}
