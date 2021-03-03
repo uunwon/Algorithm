42b771423ae76981d517001511b663bb7ba8673c
yunwoon <chea2089@naver.com>
2021-03-03 15:34:35 +0900

    [codeup] 입출력 문자 1개

---
+++ CodeUp/1011.java
@@ -0,0 +1,11 @@
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char ch = scanner.next().charAt(0);
+		
+		System.out.println(ch);
+	}
+}
