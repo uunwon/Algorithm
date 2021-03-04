69f73f66a716b1a628ac30b5877c936831c62664
yunwoon <chea2089@naver.com>
2021-03-04 21:39:09 +0900

    [codeup] 문자 2개 순서변경

---
+++ CodeUp/1014.java
@@ -0,0 +1,12 @@
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+		char ch2 = sc.next().charAt(0);
+		
+		System.out.println(ch2 + " " + ch);
+	}
+}
