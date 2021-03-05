19a61ef5fbc23ce54f57447126a4e934328f5e95
yunwoon <chea2089@naver.com>
2021-03-05 22:34:08 +0900

    [codeup] 입출력 0305

---
+++ CodeUp/1017.java
@@ -0,0 +1,13 @@
+// 정수 1개 입력받아 3번 출력하기
+
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		System.out.print(num + " " + num + " " + num);
+	}
+}
