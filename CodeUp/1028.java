a6d2c2dc39efb82a51cde209b083a5fc0f229b78
yunwoon <chea2089@naver.com>
2021-03-06 18:41:09 +0900

    [codeup] 데이터형 0306

---
+++ CodeUp/1028.java
@@ -0,0 +1,13 @@
+// 정수 1개 입력받아 그대로 출력하기2(설명)
+
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num = sc.nextLong();
+		System.out.println(num);
+	}
+}
