eab542f171103bd29b4a168baf800334f937890e
yunwoon <chea2089@naver.com>
2021-03-09 22:12:34 +0900

    [codeup] 비트단위논리연산 0309

---
+++ CodeUp/1059.java
@@ -0,0 +1,14 @@
+// 비트단위로 NOT 하여 출력하기(설명)
+
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		
+		System.out.println(~num);
+	}
+}
---
+++ CodeUp/1061.java
@@ -0,0 +1,15 @@
+// 비트단위로 OR 하여 출력하기(설명)
+
+package projectJob;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a | b);
+	}
+}
