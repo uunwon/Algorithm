13e1b600d8cd084a3d9e481400d4d9442dea2e0a
yunwoon <chea2089@naver.com>
2021-03-19 11:48:43 +0900

    [scf] 모의 - 근묵자흑 수정

--- startup coding festival 2021/[모의테스트] 근묵자흑.java
+++ startup coding festival 2021/[모의테스트] 근묵자흑.java
@@ -4,30 +4,44 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+	public static void countNumber(int[] num, int k) {
+		Stack<Integer> stack = new Stack<>();
+		int temp = 0;
+		int count = 0;
+		
+		for(int i = 0; i < num.length; i++) {
+			if(stack.size() != k)
+				stack.push(num[i]);
+			
+			if(stack.size() == k) {
+				temp = stack.pop();
+				stack.clear();
+				count++;
+				stack.push(temp);
+			}
+		}
+		
+		if(stack.size() > 1)
+			System.out.println(count + 1);
+		else
+			System.out.println(count); // 하나 남았다는 건 공통적으로 근묵자흑 성공했다는 거!
+									   // 물들일 숫자가 남아있지 않다는 말!
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
 		StringTokenizer st = new StringTokenizer(input);
+		int[] num = new int[Integer.parseInt(st.nextToken())]; // 배열 할당
 		Stack<Integer> stack = new Stack<>();
-		int temp = 0;
 		
-		int size = Integer.parseInt(st.nextToken()); // 배열 크기
 		int k = Integer.parseInt(st.nextToken()); // 선택 정수 개수
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < size; i++)
-			stack.add(Integer.parseInt(st.nextToken()));
+		for(int i = 0; i < num.length; i++)
+			num[i] = Integer.parseInt(st.nextToken());
 		
-		while (!stack.empty()) {
-			if(stack.size() == 1)
-				break;
-			
-			for(int i = 0; i < k - 1; i++) {
-				stack.pop();
-			}
-			temp++;
-		}
-		
-		System.out.println(temp);
+		countNumber(num, k);
 	}
 }
