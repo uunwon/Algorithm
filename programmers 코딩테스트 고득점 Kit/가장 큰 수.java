2ee2083c0306e965bce05c9dd6a021c4552071a2
yunwoon <chea2089@naver.com>
2021-03-24 19:20:47 +0900

    [programmers] sort 02 수정

--- programmers 코딩테스트 고득점 Kit/가장 큰 수.java
+++ programmers 코딩테스트 고득점 Kit/가장 큰 수.java
@@ -3,32 +3,28 @@ package projectJob;
 import java.util.*;
 // comparator 와 comparable 
 // https://gmlwjd9405.github.io/2018/09/06/java-comparable-and-comparator.html
-// 참고 https://ivory-room.tistory.com/43
+
 class Solution {
 	public String solution(int[] numbers) {
 		String result = "";
-		// int[] temp = new int[numbers.length];
+		String[] temp = new String[numbers.length];
 		
-		// while(true) {
-		// 맨 앞자리 비교해서 나열하고
-		// 같으면 앞자리 포함해서 비교해서 나열
-			for(int i = 0; i < numbers.length; i++) {
-				char temp = Integer.toString(numbers[i]).charAt(0);
-				int temp2;
-				
-				for(int j = i; j < numbers.length; j++) {
-					if(temp > Integer.toString(numbers[j]).charAt(0)) {
-						temp2 = numbers[j];
-						numbers[j] = numbers[i];
-						temp = Integer.toString(temp2).charAt(0);
-					}
-				}
+		for (int i = 0; i < numbers.length; i++) {
+			temp[i] = String.valueOf(numbers[i]);
+		}
+		
+		Arrays.sort(temp, new Comparator<String>() {
+			@Override
+				public int compare(String a, String b) {
+					return (b+a).compareTo(a+b);
 			}
-			
-			for(int i = 0; i < numbers.length; i++) {
-				System.out.println(numbers[i]);
-			}
-		// }
+		});
+		
+		if(temp[0].equals("0"))
+			return "0";
+		
+		for(String s : temp) 
+			result += s;
 		
 		return result;
 	}
@@ -37,7 +33,7 @@ class Solution {
 class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		int[] numbers = {6, 10, 2};
+		int[] numbers = {3, 30, 34, 5, 9};
 		
 		System.out.println(sol.solution(numbers));
 	}
