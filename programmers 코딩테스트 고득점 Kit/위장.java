5b5758a702a0cfb0e42c3c2316b5d8f6a0010ade
yunwoon <chea2089@naver.com>
2021-04-03 16:38:23 +0900

    [programmers] hash 03 수정

--- programmers 코딩테스트 고득점 Kit/위장.java
+++ programmers 코딩테스트 고득점 Kit/위장.java
@@ -4,20 +4,23 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = clothes.length;
+        int answer = 1;
         HashMap <String, Integer> hm = new HashMap<>();
         
-        for(int i = 0; i < clothes.length; i++) {
+        for(int i = 0; i < clothes.length; i++) { // < 의상, 갯수 >
         	String s = clothes[i][1];
-        	if(hm.containsKey(s)) hm.replace(s, hm.get(s) + 1);
-        	else hm.put(s, 1);
+        	
+        	if(hm.containsKey(s)) 
+        		hm.replace(s, hm.get(s) + 1);
+        	else 
+        		hm.put(s, 1);
         }
         
         for(String s : hm.keySet()) {
-        	System.out.println(s + " " + hm.get(s));
+        	answer *= (hm.get(s) + 1);
         }
         
-        return answer;
+        return answer - 1;
     }
 }
 
