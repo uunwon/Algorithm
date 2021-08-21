73e9df194bb906fc91d5cbdd2633175d30576ac3
yunwoon <chea2089@naver.com>
2021-08-21 22:22:00 +0900

    arrange folders

---
+++ practice-room/code.kt
@@ -0,0 +1,7 @@
+// 10871 번 풀면서 
+import java.util.*;
+import java.math.*;
+
+fun main() = with(Scanner(System.`in`)){
+    println(nextInt() + nextInt())
+}
--- practice-room/example.kt
+++
@@ -1,5 +0,0 @@
-// 10871 번 풀면서 
-
-fun main() {
-    println("hello world!")
-}
