7d93ce3ac12ab614fb4fa20c1a1c0f46c963b233
yunwoon <chea2089@naver.com>
2021-09-22 07:30:55 +0900

    java day-17

--- baekjoon online judge/8958 OX퀴즈.java
+++ baekjoon online judge/8958 OX퀴즈.java
@@ -1,18 +1,28 @@
-package javaPractice;
 import java.io.*;
-import java.util.*;
 
-public class code {
+public class Main {
     public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int cnt = Integer.parseInt(br.readLine());
 
 		for(int i = 0; i < cnt; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int len = Integer.parseInt(st.nextToken());
+			int score = 0;
+			int tmp = 0;
 
-			for(int j = 0; j < )
+			for(byte b : br.readLine().getBytes()) {
+				if(b == 'O') {
+					tmp++;
+					score += tmp;
+				}
+				else
+					tmp = 0;
+
+			}
+
+			sb.append(score).append("\n");
 		}
+
+		System.out.println(sb);
 	}
 }
